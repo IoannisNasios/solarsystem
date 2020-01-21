8c608cad857b2f35fccc3d718d07c2dbf59f6545
IoannisNasios <nasioannis@hotmail.com>
2020-01-21 11:59:27 +0200

    modify setup.py

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ setuptools.setup(
     version="0.1.0",
     author="Ioannis Nasios",
     author_email="nasioannis5@gmail.com",
-    description="Out Solar System. Planet's positions, Sun's anf Moon' s position and rise/set",
+    description="Out Solar System. Planet's positions, Sun's and Moon' s position and rise/set",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/IoannisNasios/solarsystem",
     packages=setuptools.find_packages(),
     classifiers=[
+	"Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.0',
+    ]
 )
