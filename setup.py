1a7575d0f234052b3fc19bd7df3018e5a804da73
IoannisNasios <nasioannis@hotmail.com>
2020-01-21 12:11:13 +0200

    modify back setup.py

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ setuptools.setup(
     url="https://github.com/IoannisNasios/solarsystem",
     packages=setuptools.find_packages(),
     classifiers=[
-	"Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
+    python_requires='>=3.0',
 )
