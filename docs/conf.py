4d6b371546f0ca7aed1961135dc77577cb9bdae8
Ioannis Nasios <nasioannis@hotmail.com>
2026-05-21 10:30:52 +0300

    update docs conf file

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.todo',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -68,8 +69,8 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-#html_theme = "sphinx_rtd_theme" # needs instalation, there are several available without need of instalation
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme' 
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
