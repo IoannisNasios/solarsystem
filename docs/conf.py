233361355fe8f9b27771a8f7dc6ae0c383e39540
Ioannis Nasios <nasioannis@hotmail.com>
2026-05-21 10:22:50 +0300

    update files

--- docs/conf.py
+++ docs/conf.py
@@ -68,8 +68,8 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-## html_theme = 'alabaster'
-#html_theme = "sphinx_rtd_theme"
+html_theme = 'alabaster'
+#html_theme = "sphinx_rtd_theme" # needs instalation, there are several available without need of instalation
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
